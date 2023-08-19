c9e70115d6c0e96808effac0213cc951a33ee09d
codeylee05 <mlefa5002@gmail.com>
2023-08-19 13:57:16 +0200

    Build continue of index page

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ window.addEventListener("mousemove", (e) => {
 
     const mouseYPosition = e.clientY;
     const viewPortHeight = window.innerHeight;
-    const thresholdHeight = 75;
+    const thresholdHeight = 93;
     const distanceFromBottom = viewPortHeight - mouseYPosition
 
     if (distanceFromBottom <= thresholdHeight) {
@@ -17,4 +17,30 @@ window.addEventListener("mousemove", (e) => {
         footerElement.style.bottom = '-100px';
     }
 
+});
+
+/*in-section-view animation*/
+// Get all feature elements
+const featureElements = document.querySelectorAll('.feature');
+
+// Define the Intersection Observer callback function
+const observerCallback = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // Add animation class to the element in view
+            entry.target.classList.add('animate');
+            // Unobserve the element to avoid redundant callbacks
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+// Create an Intersection Observer
+const observer = new IntersectionObserver(observerCallback, {
+    threshold: 0.3 // Adjust this threshold as needed
+});
+
+// Observe each feature element
+featureElements.forEach(feature => {
+    observer.observe(feature);
 });
